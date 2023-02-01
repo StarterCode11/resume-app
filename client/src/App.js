8d035a53e5184831dadd837000b4a6570d5fda77
StarterCode11 <infokumarpratyush03@gmail.com>
2023-02-01 23:25:25 +0530

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Form from "./components/Form";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-center">"Elevate Your Career with a Professional Resume Builder"</h1>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
+
